07c685230d17a9218e35d8733e0fbfa24edc233d
Shou Takenaka <sasaminn@gmail.com>
2015-02-15 02:27:26 +0900

    Add script to create pull request for production deployment

---
+++ merge_to_production_branch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Creating pull request..."
+STAGING_BRANCH="deploy/production"
+PRODUCTION_BRANCH="deploy/production"
+DATE=`date +'%Y-%m-%d %H:%M:%S'`
+URL=`hub pull-request -m "$DATE Production deployment" -b $PRODUCTION_BRANCH -h $STAGING_BRANCH`
+if [ $? -eq 0 ]; then
+    echo "Pull request created (url='$URL')."
+else
+    echo "Failed to create pull request."
+    exit 1
+fi
+
+echo "Merging pull request..."
+CURRENT_BRANCH=`git symbolic-ref --quiet --short HEAD 2> /dev/null`
+git checkout "$PRODUCTION_BRANCH" \
+    && hub merge "$URL" \
+    && git push origin "$PRODUCTION_BRANCH" \
+    && git checkout "$CURRENT_BRANCH"
+if [ $? -eq 0 ]; then
+    echo "Pull request merged."
+else
+    echo "Failed to merge pull request."
+    exit 1
+fi
